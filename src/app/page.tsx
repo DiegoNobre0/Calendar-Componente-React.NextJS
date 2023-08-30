ed69962c1e57e307217f868ac4ba5e9c3e6f55e2
Diego Nobre <diegonobre.dev@outlook.com>
2023-08-29 21:02:58 -0300

    Ajuste de layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,14 +10,8 @@ import TextField, { OutlinedTextFieldProps } from '@mui/material/TextField';
 import InputAdornment from '@mui/material/InputAdornment';
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
-import React, { useRef, useState, useEffect } from "react";
-import { useDrop } from "react-dnd";
+import React, {useRef, useState} from "react";
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
-// import { Calendar } from 'primereact/calendar';
-import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
-
-
-
 
 export default function Home() {
 
@@ -28,57 +22,66 @@ const dataFim: any = new Date('2023-02-25');
 const mesesAbreviados = ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'];
 const diasAbreviados = ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb'];
 
-  const intervalo = Math.floor((dataFim - dataInicio) / (1000 * 60 * 60 * 24));
-  const datasIntervalo = [];
+const intervalo = Math.floor((dataFim - dataInicio) / (1000 * 60 * 60 * 24));
+const datasIntervalo = [];
   
-  for (let i = 1; i <= intervalo; i++) {
-    const data = new Date(dataInicio);
-    data.setDate(dataInicio.getDate() + i);
-    datasIntervalo.push(data);
-  }
+for (let i = 1; i <= intervalo; i++) {
+  const data = new Date(dataInicio);
+  data.setDate(dataInicio.getDate() + i);
+  datasIntervalo.push(data);
+}
 
-  const dayWidth = 60;
+const dayWidth = 60;
 
-  const checkInDate = new Date('2023-02-04');
-  const checkOutDate = new Date('2023-02-09');
+const checkInDate = new Date('2023-02-04');
+const checkOutDate = new Date('2023-02-09');
 
-  const checkInIndex = datasIntervalo.findIndex(date => date.getTime() === checkInDate.getTime());
-  const checkOutIndex = datasIntervalo.findIndex(date => date.getTime() === checkOutDate.getTime());
+const checkInIndex = datasIntervalo.findIndex(date => date.getTime() === checkInDate.getTime());
+const checkOutIndex = datasIntervalo.findIndex(date => date.getTime() === checkOutDate.getTime());
 
-  const [guestData, setGuestData] = useState({
-    checkIn: checkInIndex,
-    checkOut: checkOutIndex,
-  });
+const [guestData, setGuestData] = useState({
+  checkIn: checkInIndex,
+  checkOut: checkOutIndex,
+});
+  
+const div1: any = useRef(null);
+const div2: any = useRef(null);
+
+const onScroll = () => {   
+  div2.current.scrollLeft = div1.current.scrollLeft;
+}
+
+
 const handleMouseDown = (event: any, isCheckIn: any) => {
-  event.preventDefault();
+event.preventDefault();
 
-  const startX = event.clientX;
-  const startLeft = (isCheckIn ? guestData.checkIn : guestData.checkOut) * dayWidth;
+const startX = event.clientX;
+const startLeft = (isCheckIn ? guestData.checkIn : guestData.checkOut) * dayWidth;
 
-  const handleMouseMove = (event: any) => {
-    const offsetX = event.clientX - startX;
-    const newLeft = startLeft + offsetX;
+const handleMouseMove = (event: any) => {
+  const offsetX = event.clientX - startX;
+  const newLeft = startLeft + offsetX;
 
-    const newDay = Math.max(1, Math.min(datasIntervalo.length, Math.floor(newLeft / dayWidth) + 1));
+  const newDay = Math.max(1, Math.min(datasIntervalo.length, Math.floor(newLeft / dayWidth) + 1));
 
-    if (isCheckIn) {
-      setGuestData({
-        checkIn: newDay,
-        checkOut: Math.max(guestData.checkOut, newDay),
-      });
-    } else {
-      setGuestData({
-        checkIn: Math.min(guestData.checkIn, newDay),
-        checkOut: newDay,
-      });
-    }
+  if (isCheckIn) {
+    setGuestData({
+    checkIn: newDay,
+    checkOut: Math.max(guestData.checkOut, newDay),
+  });
+  } else {
+    setGuestData({
+    checkIn: Math.min(guestData.checkIn, newDay),
+    checkOut: newDay,
+  });
+  }
     console.log(guestData.checkIn, guestData.checkOut)
-  };
+};
 
-  const handleMouseUp = () => {
-    window.removeEventListener('mousemove', handleMouseMove);
-    window.removeEventListener('mouseup', handleMouseUp);
-  };
+const handleMouseUp = () => {
+  window.removeEventListener('mousemove', handleMouseMove);
+  window.removeEventListener('mouseup', handleMouseUp);
+};
 
   window.addEventListener('mousemove', handleMouseMove);
   window.addEventListener('mouseup', handleMouseUp);
@@ -92,24 +95,24 @@ const handleGuestMouseDown = (event: any) => {
   const startLeft = (guestData.checkIn - 1) * dayWidth;
 
   const handleMouseMove = (event: any) => {
-    const offsetX = event.clientX - startX;
-    const newLeft = startLeft + offsetX;
+  const offsetX = event.clientX - startX;
+  const newLeft = startLeft + offsetX;
 
-    const newCheckIn = Math.max(1, Math.min(datasIntervalo.length - (guestData.checkOut - guestData.checkIn), Math.floor(newLeft / dayWidth) + 1));
-    const newCheckOut = newCheckIn + (guestData.checkOut - guestData.checkIn);
+  const newCheckIn = Math.max(1, Math.min(datasIntervalo.length - (guestData.checkOut - guestData.checkIn), Math.floor(newLeft / dayWidth) + 1));
+  const newCheckOut = newCheckIn + (guestData.checkOut - guestData.checkIn);
 
-    setGuestData({
-      checkIn: newCheckIn,
-      checkOut: newCheckOut,
-    });
+  setGuestData({
+    checkIn: newCheckIn,
+    checkOut: newCheckOut,
+  });
 
-    console.log(guestData.checkIn, guestData.checkOut)
+  console.log(guestData.checkIn, guestData.checkOut)
   };
 
-  const handleMouseUp = () => {
-    window.removeEventListener('mousemove', handleMouseMove);
-    window.removeEventListener('mouseup', handleMouseUp);
-  };
+const handleMouseUp = () => {
+  window.removeEventListener('mousemove', handleMouseMove);
+  window.removeEventListener('mouseup', handleMouseUp);
+};
 
   window.addEventListener('mousemove', handleMouseMove);
   window.addEventListener('mouseup', handleMouseUp);
@@ -143,15 +146,12 @@ const toggleAccordion = () => {
 
 return (
 <main className={styles.main}>
-    <div style={{display:'flex'}}>
-    <div style={{backgroundColor:'red'}}>
-      teste
-    </div>
+    <div style={{display:'flex'}}>   
     <div >
-      <div style={{display: 'flex', width:'80rem'}}>
-        <div style={{width:'10rem', backgroundColor:'#fff', borderRight: 'solid 3px'}}></div>
+      <div style={{display: 'flex', width:'80rem', overflow: 'auto'}}>
+        <div style={{width:'10rem', backgroundColor:'#fff', borderRight: 'solid 3px'}}>TESTE</div>
         <div className={styles.calendar}>
-        <div className={styles.daysContainer}>
+        <div className={styles.daysContainer} ref={div1} onScroll={onScroll}>
           {datasIntervalo.map((date, index) => (
             <div
               key={index}
@@ -175,55 +175,57 @@ return (
         <ArrowDropDownIcon></ArrowDropDownIcon>
       </div>
       {accordionOpen && (
-   
-          <div className={styles.calendar}>
-          <div className={styles.daysContainer}>
-            {datasIntervalo.map((date, index) => (
+        <div style={{display:'flex',overflow: 'auto'}}>
+            <div style={{width:'10rem', backgroundColor:'#fff', borderRight: 'solid 3px'}}>TESTE</div>
+            <div className={styles.calendar} >
+            <div className={styles.daysContainer} ref={div2} onScroll={onScroll}>
+              {datasIntervalo.map((date, index) => (
+                <div
+                  key={index}
+                  className={`${styles.day} ${styles.draggingOver}`}
+                  onDragOver={(event) => event.preventDefault()}
+                  onDrop={(event) => handleDayDrop(event, index)}
+                >
+                  <span>{mesesAbreviados[date.getMonth()]}</span>
+                  <span style={{ textAlign: 'center' }}>{date.getDate()}</span>
+                  <span>{diasAbreviados[date.getDay()]}</span>
+                </div>
+              ))}
               <div
-                key={index}
-                className={`${styles.day} ${styles.draggingOver}`}
-                onDragOver={(event) => event.preventDefault()}
-                onDrop={(event) => handleDayDrop(event, index)}
-              >
-                <span>{mesesAbreviados[date.getMonth()]}</span>
-                <span style={{ textAlign: 'center' }}>{date.getDate()}</span>
-                <span>{diasAbreviados[date.getDay()]}</span>
-              </div>
-            ))}
-            <div
-              className={`${styles.guest} ${styles.draggingGuest}`}
-              style={{
-                left: `${guestData.checkIn * dayWidth}px`,
-                width: `${(guestData.checkOut - guestData.checkIn + 1) * dayWidth}px`,
-                cursor: 'move',
-                display: 'flex',
-                position: 'absolute',
-              }}
-              draggable
-              onDragStart={handleGuestDragStart}
-              onDragEnd={handleGuestDragEnd}
-            >
-              <div
-                className={styles.checkInOut}
+                className={`${styles.guest} ${styles.draggingGuest}`}
                 style={{
-                  left: `0`,
-                  width: `100%`,
-                  cursor: 'col-resize',
+                  left: `${guestData.checkIn * dayWidth}px`,
+                  width: `${(guestData.checkOut - guestData.checkIn + 1) * dayWidth}px`,
+                  cursor: 'move',
+                  display: 'flex',
+                  position: 'absolute',
                 }}
-                onMouseDown={(e) => handleMouseDown(e, true)}
+                draggable
+                onDragStart={handleGuestDragStart}
+                onDragEnd={handleGuestDragEnd}
               >
-                in
-              </div>
-              <div
-                className={styles.checkInOut}
-                style={{
-                  left: `0`,
-                  width: `14%`,
-                  cursor: 'col-resize',
-                }}
-                onMouseDown={(e) => handleMouseDown(e, false)}
-              >
-                out
+                <div
+                  className={styles.checkInOut}
+                  style={{
+                    left: `0`,
+                    width: `100%`,
+                    cursor: 'col-resize',
+                  }}
+                  onMouseDown={(e) => handleMouseDown(e, true)}
+                >
+                  in
+                </div>
+                <div
+                  className={styles.checkInOut}
+                  style={{
+                    left: `0`,
+                    width: `14%`,
+                    cursor: 'col-resize',
+                  }}
+                  onMouseDown={(e) => handleMouseDown(e, false)}
+                >
+                  out
+                </div>
               </div>
             </div>
           </div>
